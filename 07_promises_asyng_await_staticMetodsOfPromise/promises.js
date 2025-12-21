8825a343d741879a0590147b0dff9e6d9e060810
Anton-013 <antonchuvashovoren@mail.ru>
2025-12-21 23:18:23 +0300

    tasks of promises

--- 07_promises_asyng_await_staticMetodsOfPromise/promises.js
+++ 07_promises_asyng_await_staticMetodsOfPromise/promises.js
@@ -1,8 +1,8 @@
 
 // TODO: дополнительно можно посмотреть, ознакомится
-// https://www.youtube.com/watch?v=3d9XqFmCmQw 
+// https://www.youtube.com/watch?v=3d9XqFmCmQw
 
-// метод finally выполняется не зависимо от того что произошло с промисом (resolve или reject) 
+// метод finally выполняется не зависимо от того что произошло с промисом (resolve или reject)
 // в него не приходят ни какие данный, в цепочку промисов его можно ставить в любом месте и несколько штук
 // основная задача finally это мутация глобальных переменных или глобального стейта
 // если в finally произойдет ошибка то следующий промис за ним удаляется и вместо него идет новый промис в состоянии reject
@@ -45,7 +45,7 @@
 
 // результат => 2 3 4 1
 
-// Task 2 
+// Task 2
 
 // new Promise((res, rej) => {
 //     console.log(1)
@@ -96,22 +96,72 @@
 
 // Task 5
 
-(function () {
-    setTimeout(() => console.log(1), 100)
-})()
-console.log(2)
+// (function () {
+//     setTimeout(() => console.log(1), 100)
+// })()
+// console.log(2)
 
-let i = 0
-while (i < 500000000) {
-    i++
-}
+// let i = 0
+// while (i < 500000000) {
+//     i++
+// }
 
-new Promise((res, rej) => {
-    setTimeout(() => console.log(3), 50)
-})
-function f() {
-    console.log(4)
-}
-Promise.resolve(console.log(5))
+// new Promise((res, rej) => {
+//     setTimeout(() => console.log(3), 50)
+// })
+// function f() {
+//     console.log(4)
+// }
+// Promise.resolve(console.log(5))
 
 // 2 5 1 3
+
+// Task 6
+
+// async function sleep(ms) {
+//     return new Promise((res, rej) => {
+//         setTimeout(() => {
+//             console.log(ms)
+//             res()
+//         }, ms * 100)
+//     })
+// }
+
+// async function show() {
+//     await sleep(3)
+//     await sleep(2)
+//     await sleep(1)
+// }
+
+// show()
+
+// const pr1 = Promise.resolve(10)
+// const pr2 = Promise.resolve(0)
+
+// pr1
+//     .then(res => {
+//         console.log(res)
+//         return res + 2
+//     })
+//     .then(res => {
+//         console.log(res)
+//         return res + 2
+//     })
+//     .then(res => {
+//         console.log(res)
+//         return res + 2
+//     })
+
+// pr2
+//     .then(res => {
+//         console.log(res)
+//         return res + 1
+//     })
+//     .then(res => {
+//         console.log(res)
+//         return res + 1
+//     })
+//     .then(res => {
+//         console.log(res)
+//         return res + 1
+//     })
